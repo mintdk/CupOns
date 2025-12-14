12e849d41508ce8a3507f92390e2550087a6b6ca
mintdk <yaritzacarmenluna89@gmail.com>
2025-12-13 23:14:41 -0400

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAU9zcn7DkI2dDuqkmBgUM_zFpY5xcbAiw",
+  authDomain: "cupons-3daf2.firebaseapp.com",
+  projectId: "cupons-3daf2",
+  storageBucket: "cupons-3daf2.firebasestorage.app",
+  messagingSenderId: "850464640588",
+  appId: "1:850464640588:web:2057540fdbdf966da1eee9"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
 function getCoupons() {
     return JSON.parse(localStorage.getItem("coupons")) || {};
 }
@@ -93,3 +105,4 @@ if (redeemBtn) {
         codeInput.value = "";
     });
 }
+
